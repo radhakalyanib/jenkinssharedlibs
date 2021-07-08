a5b7e3dc200c8fb9add134702bbaabf282db71a7
Cloud User <ec2-user@ip-172-31-59-182.ec2.internal>
2021-07-08 13:28:19 +0000

    sync

--- vars/runargocd.groovy
+++ vars/runargocd.groovy
@@ -1,6 +1,6 @@
 def call(appname) {
 echo "Application name :  ${appname}"
-def proc = "/home/linuxbrew/.linuxbrew/bin/argocd app get ${appname}".execute();
+def proc = "/home/linuxbrew/.linuxbrew/bin/argocd app sync ${appname}".execute();
 def outputStream = new StringBuffer();
 proc.waitForProcessOutput(outputStream, System.err)
 //proc.waitForProcessOutput(System.out, System.err)
