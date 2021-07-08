3a8602e9796d9d1235c6dbd51a009d50c2dfb960
Cloud User <ec2-user@ip-172-31-59-182.ec2.internal>
2021-07-08 10:59:48 +0000

    get argocd jobs details

--- vars/runargocd.groovy
+++ vars/runargocd.groovy
@@ -1,3 +1,8 @@
 def call(appname) {
 echo "Application name :  ${appname}"
+def proc = "argocd app get ${appname}".execute();
+def outputStream = new StringBuffer();
+proc.waitForProcessOutput(outputStream, System.err)
+//proc.waitForProcessOutput(System.out, System.err)
+
 }
